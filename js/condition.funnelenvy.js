af9b43ca3cc90d2504ff04441f270ea22e56ec53
“subhashbrillmark” <subhash@brillmark.com>
2020-07-15 17:46:35 +0530

    pending promise

--- js/condition.funnelenvy.js
+++ js/condition.funnelenvy.js
@@ -12,7 +12,7 @@
             if (attempts < 200) {
               if (window.funnelEnvy && typeof window.funnelEnvy.on === 'function') {
                 clearInterval(interval);
-                resolve(true);
+                resolve(window.funnelEnvy);
               }
             }
             else {
@@ -27,21 +27,26 @@
   Drupal.smartContent.plugin.Field['funnelenvy'] = function (condition) {
     let key = condition.field.pluginId.split(':')[1];
         Drupal.smartContent.funnelenvy = new Promise((resolve, reject) => {
-          Promise.resolve(Drupal.smartContent.waitForFunnelenvy).then(function(isFeAvailable){
-            if(isFeAvailable){
+          Promise.resolve(Drupal.smartContent.waitForFunnelenvy).then(function(Funnelenvy){
+            if(Funnelenvy){
               if(key === 'variationSlug'){
-                window.funnelEnvy.addListener('backstage.activeVariation', function(model, message) {
+                Funnelenvy.addListener('backstage.activeVariation', function(model, message) {
                   if((model && model.event) === 'backstage.activeVariation'){
                     resolve(model.backstage.activeVariation)
                   }
                 });
               }else{
-                window.funnelEnvy.addListener('backstage.updatedAudiences', function(model, message) {
+                Funnelenvy.addListener('backstage.updatedAudiences', function(model, message) {
                   if((model && model.event) === 'backstage.updatedAudiences'){
                     resolve(model.backstage.audiences[condition.settings.value])
                   }
                 });
               } 
+
+              setTimeout(function(){
+                resolve(false)
+              },4000);
+
             }else{
               resolve(false);
             }
